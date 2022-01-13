aba898ff5068eff103fa846cad65ffcbb5559c13
Blessonpx <36164387+Blessonpx@users.noreply.github.com>
2022-01-13 21:09:40 +0530

    Update linked_list.js

--- linked_list.js
+++ linked_list.js
@@ -72,6 +72,25 @@ class linkedList{
     this.length--;
     return this.printList()
   }
+    reverse() {
+      if (!this.head.next) {
+        return this.head;
+      }
+      let first = this.head;
+      this.tail = this.head;
+      let second = first.next;
+  
+      while(second) {
+        const temp = second.next;
+        second.next = first;
+        first = second;
+        second = temp;
+      }
+  
+      this.head.next = null;
+      this.head = first;
+      return this.printList();
+    }
 }
 
 class doubly_linkedList{

846f4172001f4c82671d87dcba2379b2496eff00
Blessonpx <36164387+Blessonpx@users.noreply.github.com>
2022-01-29 12:57:31 +0530

    Update hacks_dsa.js

--- hacks_dsa.js
+++ hacks_dsa.js
@@ -1,7 +1,7 @@
 // Check if a word is a Palindrome or not
 // Use Arrays of 0's to count 
 
-function palinCheck(c){
+function palinCheck_array(c){
   x = new Array(26).fill(0)
   for (let i=0; i<c.length;i++){
   x[c.charCodeAt(i)-'a'.charCodeAt(0)]++;}
@@ -11,5 +11,26 @@ function palinCheck(c){
   }
   return count<=1;
 }
+
+function palinCheck_hash(c){
+  // simple hash
+  let hash = {}
+  let charcount=0;
+  for(let i=0;i<c.length;i++){
+    if(hash[c[i]]){
+      delete hash[c[i]]
+    }else {
+      hash[c[i]]=true
+    }
+    charcount++
+  }
+  if(charcount%2===0){
+    return Object.keys(hash).length===0;
+  } else {
+    return Object.keys(hash).length===1;
+  }
+}
+
 let r='raca'
-console.log(palinCheck(r))
+console.log(palinCheck_array(r))
+console.log(palinCheck_hash(r))
